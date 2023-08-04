0b6ea4dbdb8da1868aa2d50807dd9c911f465142
volodymyr-storozhuk <volodymyr.storozhuk@hotmail.com>
2023-08-04 16:42:58 +0300

    add verify create table to main

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from aiogram import executor
 from src.echobot import dp
 # for run as script
 # from src.echobot import dp
+from src.database.create_table import execute_query, create_message_table
 
 
 if __name__ == "__main__":
+    execute_query(create_message_table)
     executor.start_polling(dp, skip_updates=True)
